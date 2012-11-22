3ea0324db98f0125981e213ab7e192cb49b56d9b
unenemy <unenemy@unenemy-laptop.(none)>
2012-11-22 23:38:29 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pzks::Application.config.secret_token = '8c8a963af84271f4666a9c52c35d2fbadbd83cb95b10060adca24780794c3cebd5a6e0e3cb9143611897cdfe5627f0db801f6988da31fb489c13d58536ada9f9'
